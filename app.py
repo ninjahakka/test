edbe3a47dcc9fc6e9c1716c3baf0f5d8032c78b4
ninjahakka <106673288+ninjahakka@users.noreply.github.com>
2022-06-04 19:44:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def handle_message(event):
                                 text= "個股資訊 " + message[3:]),
                             MessageAction( 
                                 label= message[3:] + " 個股新聞",
-                                text= "個股新聞 " + message[3:])
+                                text= "個股新聞 " + message[3:]),
                             MessageAction( 
                                 label= "不清楚",
                                 text= "個股新聞 " + message[3:])
