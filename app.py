deac3bacdaab26209b6128bc1af995ac94d344a5
ninjahakka <106673288+ninjahakka@users.noreply.github.com>
2022-06-04 18:22:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ def handle_message(event):
                     ]
                 ) 
             )
-        line_bot_api.reply_message(event.reply_token, buttons_template_message)
+    line_bot_api.reply_message(event.reply_token, buttons_template_message)
 	
     
 #主程式
