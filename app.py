25904f1b174fe997c55c4f502ecfa3125b9f4296
ninjahakka <106673288+ninjahakka@users.noreply.github.com>
2022-06-04 15:46:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def callback():
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+    message = TextSendMessage(text="KKBOX"+event.message.text)
     line_bot_api.reply_message(event.reply_token,message)
     
 #主程式
