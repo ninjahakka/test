662e2425d019df7141b4779af277d9a0445b6734
ninjahakka <106673288+ninjahakka@users.noreply.github.com>
2022-06-08 20:57:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -71,19 +71,19 @@ def handle_message(event):
         message = function_list()
         line_bot_api.reply_message(event.reply_token, message)
     elif '我是學生啦' in msg:
-        message = TextSendMessage(text='1️⃣自主回報在校密切接觸者\n學校將聯絡衛生局開立居家隔離單\n☎️ 回報專線：02-29393091 \n\n（平日/上班時間）\n身健中心分機：77424 or 77425 \n（假日/非上班時間）\n學安中心分機：66119 or 0919099119 \n\n2️⃣ 請進行居家照護7天＋自主健康管理7天\n（自主健康管理期間做好個人防護可入校）\n3⃣️ 至台灣社交距離APP回報足跡')
+        message = TextSendMessage(text='1️⃣ 自主回報在校密切接觸者\n學校將聯絡衛生局開立居家隔離單\n☎️ 回報專線：02-29393091 \n\n（平日/上班時間）\n身健中心分機：77424 or 77425 \n（假日/非上班時間）\n學安中心分機：66119 or 0919099119 \n\n2️⃣ 請進行居家照護7天＋自主健康管理7天\n（自主健康管理期間做好個人防護可入校）\n3⃣️ 至台灣社交距離APP回報足跡')
         line_bot_api.reply_message(event.reply_token, message)
     elif '我是教職員啦' in msg:
-        message = TextSendMessage(text='1. 自主回報在校密切接觸者\n學校將聯絡衛生局開立居家隔離單\n☎️ 回報專線：02-29393091\n\n（平日/上班時間）\n環安組分機：62823 \n（假日/非上班時間）\n學安中心分機：66119 or 0919099119\n\n2️⃣ 請進行居家照護7天＋自主健康管理7天\n（自主健康管理期間做好個人防護可入校）\n3⃣️ 至台灣社交距離APP回報足跡')
+        message = TextSendMessage(text='1️⃣ 自主回報在校密切接觸者\n學校將聯絡衛生局開立居家隔離單\n☎️ 回報專線：02-29393091\n\n（平日/上班時間）\n環安組分機：62823 \n（假日/非上班時間）\n學安中心分機：66119 or 0919099119\n\n2️⃣ 請進行居家照護7天＋自主健康管理7天\n（自主健康管理期間做好個人防護可入校）\n3⃣️ 至台灣社交距離APP回報足跡')
         line_bot_api.reply_message(event.reply_token, message)
     elif '我是密切接觸者' in msg:
-        message = TextSendMessage(text='您有以下選擇：\n 1️⃣ 居家隔離3天＋自主防疫4天（不可入校、在宿舍不可外出）\n 2️⃣  如果您是疫苗打滿三劑且快篩陰性者 可直接自主防疫七天（不可入校、在宿舍不可外出）')
+        message = TextSendMessage(text='您有以下選擇：\n 1️⃣ 居家隔離3天＋自主防疫4天（不可入校、在宿舍不可外出）\n 2️⃣ 如果您是疫苗打滿三劑且快篩陰性者 可直接自主防疫七天（不可入校、在宿舍不可外出）')
         line_bot_api.reply_message(event.reply_token, message)
     elif '我是自我應變者' in msg:
         message = TextSendMessage(text='請自主監測3天 期間內不可入校，暫停實體上課、上班')
         line_bot_api.reply_message(event.reply_token, message)
     elif '我是自主健康管理者' in msg:
-        message = TextSendMessage(text='1️⃣  如果無任何症狀可正常生活，但外出請佩戴醫用口罩 \n2️⃣ 避免出入無法保持社交距離，或容易近距離接觸不特定人之場所 \n3⃣️ 禁止與他人進行近距離或群聚行之活動（例如：聚餐、聚會、公眾集會等）\n4⃣️ 禁止前往醫院陪病，亦應延後非急迫性需求之醫療或檢查')
+        message = TextSendMessage(text='1️⃣ 如果無任何症狀可正常生活，但外出請佩戴醫用口罩 \n2️⃣ 避免出入無法保持社交距離，或容易近距離接觸不特定人之場所 \n3⃣️ 禁止與他人進行近距離或群聚行之活動（例如：聚餐、聚會、公眾集會等）\n4⃣️ 禁止前往醫院陪病，亦應延後非急迫性需求之醫療或檢查')
         line_bot_api.reply_message(event.reply_token, message)
     elif '不確定定義' in msg:
         message = TextSendMessage(text='1️⃣ 確診者：PCR陽性 or 經醫師視訊評估快篩陽性 \n2️⃣ 密切接觸者：確診者的同住親友 or 同寢室友\n3⃣️ 自主應變者：與確診者前2日有任一方摘下口罩進行15分鐘以上接觸 \n4⃣️ 自主健康管理者：確診者進行居家照護7日後的7日為自主健康管理期間')
@@ -95,7 +95,7 @@ def handle_message(event):
         message = Carousel_Template_1()
         line_bot_api.reply_message(event.reply_token, message)
     elif '世誠診所線上預約看診流程' in msg:
-        message = TextSendMessage(text='1.加入世誠診所官方帳號\n2.在對話欄寫上看診需求\n a.已 PCR 確診(日期)，想看診開藥\n b.剛快篩陽性，想確認並上傳疾管局及看診開藥\n3.初診者，可先提供健保卡及聯絡電話，加快掛號速度。')
+        message = TextSendMessage(text='1️⃣ 加入世誠診所官方帳號\n2️⃣ 在對話欄寫上看診需求\n a.已 PCR 確診(日期)，想看診開藥\n b.剛快篩陽性，想確認並上傳疾管局及看診開藥\n3⃣️ 初診者，可先提供健保卡及聯絡電話，加快掛號速度。')
         line_bot_api.reply_message(event.reply_token, message)
     elif '世誠診所電話號碼' in msg:
         message = TextSendMessage(text='02-22343618')
@@ -107,7 +107,7 @@ def handle_message(event):
         message = TextSendMessage(text='02-29364708')
         line_bot_api.reply_message(event.reply_token, message)  
     elif '定安診所線上預約看診流程' in msg:
-        message = TextSendMessage(text='1.於 LINE 線上諮詢，安排看診時間。\n2.醫師於約定時間致電\n3.使用 LINE 視訊看診\n4.請親友攜帶健保卡來繳費領藥')
+        message = TextSendMessage(text='1️⃣ 於 LINE 線上諮詢，安排看診時間。\n2️⃣ 醫師於約定時間致電\n3⃣️ 使用 LINE 視訊看診\n4.請親友攜帶健保卡來繳費領藥')
         line_bot_api.reply_message(event.reply_token, message)
     elif '定安診所電話號碼' in msg:
         message = TextSendMessage(text='02-29323755')
@@ -123,7 +123,7 @@ def handle_message(event):
         message = Carousel_Template_2() #原本誤植為Confirm_Template
         line_bot_api.reply_message(event.reply_token, message)
     elif '喉嚨沙啞' in msg:
-        message = TextSendMessage(text='建議您可以食用下列食品，來紓緩喉嚨沙啞的症狀喔！\n1.蜂蜜水\n2.喝水\n3.喉嚨噴劑\n4.含喉片\n5.清冠一號')
+        message = TextSendMessage(text='建議您可以食用下列食品，來紓緩喉嚨沙啞的症狀喔！\n1️⃣ 蜂蜜水\n2️⃣ 喝水\n3️⃣ 喉嚨噴劑\n4️⃣ 含喉片\n5️⃣ 清冠一號')
         line_bot_api.reply_message(event.reply_token, message)
     elif '極度疲勞' in msg:
         message = TextSendMessage(text='建議您可以嘗試以下方式，來紓緩極度疲勞的症狀喔！\n1.充足的睡眠和休息，提升自身免疫力。\n2.維生素A以及維生素D以強化黏膜健康，幫助提升免疫功能。\n3.喝電解水。')
